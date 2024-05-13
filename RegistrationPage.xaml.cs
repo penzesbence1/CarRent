f9789d21418b5bfae0e52aefb8d6f22ee0c1ce5d
penzesbence1 <pebex0604@gmail.com>
2024-05-13 19:02:51 +0200

    Egér a gombokon kézzé váljon

--- RegistrationPage.xaml.cs
+++ RegistrationPage.xaml.cs
@@ -199,5 +199,26 @@ namespace CarRent
             }
            
         }
+
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            Cursor = Cursors.Hand;
+
+
+
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+
+            Cursor = Cursors.Arrow;
+
+
+        }
+
+
     }
 }
